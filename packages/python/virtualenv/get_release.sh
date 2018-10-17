546511d37871957e88b4c36c0d1cbe47b832062b
Whisperity <whisperity@gmail.com>
2018-10-17 19:59:43 +0200

    Add sharkdp/fd as a tool to download and install

--- packages/python/virtualenv/get_release.sh
+++ packages/python/virtualenv/get_release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-curl -s https://api.github.com/repos/pypa/virtualenv/tags \
+curl -sL http://api.github.com/repos/pypa/virtualenv/tags \
   | grep "tarball" \
   | head -n 1 \
-  | cut -d '"' -f 4 \
+  | cut -d ":" -f 2,3 \
+  | sed -E 's/^ "(.*)",?$/\1/' \
   | wget -O virtualenv.tar.gz -i -
