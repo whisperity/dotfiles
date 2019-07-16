4deb0da0e9166825e549201e4a9406a0f96893df
Whisperity <whisperity@gmail.com>
2019-07-16 06:12:47 +0200

    Install http://github.com/ogham/exa as a "fancy tool"

---
+++ packages/tools/fancy/exa.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ARCH=$(arch)
+
+curl -sL http://api.github.com/repos/ogham/exa/releases/latest \
+  | grep "exa-linux-" \
+  | grep "${ARCH}" \
+  | grep ".zip" \
+  | grep "browser_download_url" \
+  | cut -d ":" -f 2,3 \
+  | sed -E 's/^ "(.*)",?$/\1/' \
+  | wget -O exa.zip -i -
+
+unzip exa.zip
+rm exa.zip
+mv exa-* exa
+
