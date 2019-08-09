4ac2f49e9b1b24c71f7655dacaeaa58cc552cadf
Whisperity <whisperity@gmail.com>
2019-08-09 18:37:51 +0200

    Add UNINSTALL actions to the current set of packages

--- packages/texlive/texlive.sh
+++ packages/texlive/texlive.sh
@@ -1,5 +1,5 @@
 # Load binaries from the TeX-Live installation.
-TEX_BINDIR="${HOME}/.local/share/texlive/*/bin/*"
+TEX_BINDIR="${HOME}/opt/texlive/*/bin/*"
 # The echo trick here is to force expansion of the * characters above.
 TEX_BINDIR=$(echo ${TEX_BINDIR} | tr ' ' '\n' | sort -V -r | head -n 1)
 
