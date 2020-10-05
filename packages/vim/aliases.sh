84c616bd24e42055b5ec61b88a51b66226463ef0
Whisperity <whisperity@gmail.com>
2020-10-05 16:58:38 +0200

    [vim] Default to dark theme properly even when other scripts (such as 'gc' alias) calls 'vim'

--- packages/vim/aliases.sh
+++ packages/vim/aliases.sh
@@ -2,6 +2,8 @@
 
 if [ -z "$VIM_ALIAS_EXPANDED" ]
 then
+    export VIM_THEME=dark
+
     alias darkvim="VIM_THEME=dark $(command -v vim) "
     alias lightvim="VIM_THEME=light $(command -v vim) "
 
