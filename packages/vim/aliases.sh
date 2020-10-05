a36e8b6630dec4408f784d6b184a9f78d5c3a1a2
Whisperity <whisperity@gmail.com>
2020-10-05 10:51:19 +0200

    [vim] Fix shell alias loading messing up and default to dark vim

--- packages/vim/aliases.sh
+++ packages/vim/aliases.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-alias darkvim="VIM_THEME=dark $(command -v vim) "
-alias lightvim="VIM_THEME=light $(command -v vim) "
 
-alias vim="lightvim"
+if [ -z "$VIM_ALIAS_EXPANDED" ]
+then
+    alias darkvim="VIM_THEME=dark $(command -v vim) "
+    alias lightvim="VIM_THEME=light $(command -v vim) "
+
+    alias vim="darkvim"
+
+    export VIM_ALIAS_EXPANDED=1
+fi
 
