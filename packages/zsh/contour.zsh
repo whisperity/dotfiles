6517d171c7abeea2a4dfe1c64db60a3c43d08e3b
Whisperity <whisperity@gmail.com>
2021-04-26 13:44:39 +0200

    [zsh] Contour: Harden the magic eval against accidentally rming something

--- packages/zsh/contour.zsh
+++ packages/zsh/contour.zsh
@@ -3,5 +3,9 @@ then
   return
 fi
 
+local CONTOUR_WHERE="$(which contour)"
+local OLD_PATH="${PATH}"
+unset PATH
 # Load the integration script from the currently installed Contour executable.
-eval "$(contour generate integration shell zsh to -)";
+eval "$(${CONTOUR_WHERE} generate integration shell zsh to -)";
+export PATH="${OLD_PATH}"
