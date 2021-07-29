d6bf664f1e457c95d098bf7644e0865315ac2703
Whisperity <whisperity@gmail.com>
2021-07-29 15:09:59 +0200

    [tools.fancy] Download only the GNU glibc version of "choose"

--- packages/tools/fancy/choose.sh
+++ packages/tools/fancy/choose.sh
@@ -5,6 +5,7 @@ curl -sL http://api.github.com/repos/theryangeary/choose/releases/latest \
   | grep "browser_download_url" \
   | cut -d ":" -f 2,3 \
   | sed -E 's/^ "(.*)",?$/\1/' \
+  | grep "gnu" \
   | wget -O choose -i -
 
 chmod -v +x ./choose
