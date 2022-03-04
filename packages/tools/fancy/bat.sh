8ca9e6689c376c47d98d6477c9d3e7385ab8f142
Whisperity <whisperity@gmail.com>
2022-03-04 11:26:33 +0100

    fix: [tools.fancy] Hardcode some versions
    Newer versions of these tools are broken on Ubuntu 18.04.

--- packages/tools/fancy/bat.sh
+++ packages/tools/fancy/bat.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-ARCH=$(arch)
-if [[ $ARCH == "x86_64" ]]
-then
-  ARCH="amd64"
-else
-  ARCH="i386"
-fi
+# ARCH=$(arch)
+# if [[ $ARCH == "x86_64" ]]
+# then
+#   ARCH="amd64"
+# else
+#   ARCH="i386"
+# fi
+#
+# curl -sL http://api.github.com/repos/sharkdp/bat/releases/latest \
+#   | grep "bat_" \
+#   | grep "_${ARCH}.deb" \
+#   | grep "browser_download_url" \
+#   | cut -d ":" -f 2,3 \
+#   | sed -E 's/^ "(.*)",?$/\1/' \
+#   | wget -O bat.deb -i -
 
-curl -sL http://api.github.com/repos/sharkdp/bat/releases/latest \
-  | grep "bat_" \
-  | grep "_${ARCH}.deb" \
-  | grep "browser_download_url" \
-  | cut -d ":" -f 2,3 \
-  | sed -E 's/^ "(.*)",?$/\1/' \
-  | wget -O bat.deb -i -
+wget -O bat.deb \
+  http://github.com/sharkdp/bat/releases/download/v0.20.0/bat_0.20.0_amd64.deb
--- packages/tools/fancy/delta.sh
+++ packages/tools/fancy/delta.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-ARCH=$(arch)
-if [[ $ARCH == "x86_64" ]]
-then
-  ARCH="amd64"
-else
-  ARCH="i386"
-fi
+# ARCH=$(arch)
+# if [[ $ARCH == "x86_64" ]]
+# then
+#   ARCH="amd64"
+# else
+#   ARCH="i386"
+# fi
 
-curl -sL http://api.github.com/repos/dandavison/delta/releases/latest \
-  | grep "git-delta_" \
-  | grep "_${ARCH}.deb" \
-  | grep "browser_download_url" \
-  | cut -d ":" -f 2,3 \
-  | sed -E 's/^ "(.*)",?$/\1/' \
-  | wget -O delta.deb -i -
+# curl -sL http://api.github.com/repos/dandavison/delta/releases/latest \
+#   | grep "git-delta_" \
+#   | grep "_${ARCH}.deb" \
+#   | grep "browser_download_url" \
+#   | cut -d ":" -f 2,3 \
+#   | sed -E 's/^ "(.*)",?$/\1/' \
+#   | wget -O delta.deb -i -
+
+wget -O delta.deb \
+  http://github.com/dandavison/delta/releases/download/0.4.0/git-delta_0.4.0_amd64.deb
