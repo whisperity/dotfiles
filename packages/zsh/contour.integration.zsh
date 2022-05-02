86cb822beb99339b9cf3fe66aa0496cca8054842
Whisperity <whisperity@gmail.com>
2022-05-02 15:34:31 +0200

    [zsh] Use a temporary file for Contour integration

--- packages/zsh/contour.integration.zsh
+++ packages/zsh/contour.integration.zsh
@@ -3,9 +3,22 @@ then
   return
 fi
 
+# echo "Generating Contour integration..." >&2
+
+local TEMPFILE=$(mktemp --suffix=".zsh")
+chmod +x ${TEMPFILE}
+
 local CONTOUR_WHERE="$(which contour)"
+local CAT_WHERE="$(which cat)"
+local RM_WHERE="$(which rm)"
 local OLD_PATH="${PATH}"
 unset PATH
+
 # Load the integration script from the currently installed Contour executable.
-eval "$(${CONTOUR_WHERE} generate integration shell zsh to -)";
+${CONTOUR_WHERE} generate integration shell zsh to "${TEMPFILE}"
+# ${CAT_WHERE} ${TEMPFILE}
+# eval $(${CAT_WHERE} ${TEMPFILE})
+source ${TEMPFILE}
+${RM_WHERE} ${TEMPFILE}
+
 export PATH="${OLD_PATH}"
