abce13239467413c33386fe23a3c33626c573974
Whisperity <whisperity@gmail.com>
2022-05-07 22:18:03 +0200

    [envprobe] Use one-liner hook as Envprobe `v1` is soon to be released

--- packages/envprobe/envprobe.sh
+++ packages/envprobe/envprobe.sh
@@ -1,9 +1,4 @@
 # Envprobe works best if this file is loaded LAST, after all and every other
 # setting, alias, extension, etc.
 
-unset  ENVPROBE_CONFIG
-export ENVPROBE_LOCATION=~/opt/envprobe
-export ENVPROBE_SHELL_PID=$$
-eval   "$(${ENVPROBE_LOCATION}/envprobe-config.py shell bash)"
-alias  ep='envprobe'
-alias  epc='envprobe-config'
+eval "$(~/opt/envprobe/envprobe config hook bash $$)";
