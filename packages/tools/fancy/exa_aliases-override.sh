f4e8b88c8a38aa61503a23cbb2c11bbf9f006522
Whisperity <whisperity@gmail.com>
2022-06-12 09:47:40 +0200

    [tools.fancy] Better `exa` aliases that do not conflict with Bash or OMZ's builtins

--- packages/tools/fancy/exa_aliases-override.sh
+++ packages/tools/fancy/exa_aliases-override.sh
@@ -1,12 +1,14 @@
 # Some ls-like aliases for exa (new 'ls')
 alias e='exa'
-alias el='exa -al'
+alias el='exa -l'
+alias ela='exa -al'
 alias elg='exa -al --git'
 alias tree='exa --tree'
 
 # Now alias ls-like stuff to 'exa' for real
 alias l='e -F'
 alias ls='e'
-alias la='el' # These things are all handled by exa by default
+alias la='ela'  # These things are all handled by exa by default
+alias ll='el'
 alias lh='el'
-
+alias lah='ela'
