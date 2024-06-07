4dca0c2c004378468c8a58e69534aa370466f32b
Whisperity <whisperity@gmail.com>
2024-06-07 18:03:59 +0200

    chore(distcc): Drop `distcc` and `distcc-worker` - factored out to new repository
    The `distcc` client package was moved to `whisperity/distcc-driver` in
    commit whisperity/distcc-driver@894d51044c5898ad7969e4598de12f07199539b5.
    Recreate the `distcc` package to download that code as an external
    package, just like `bash.git-prompt` or `zsh.powerline` and similar.
    
    The `distcc-docker` service worker code, never part of a managed
    package, was moved to http://github.com/whisperity/distcc-docker, and
    then redeveloped.
    As exists in the current repository, that code had serious issues, e.g.,
    `cron` never worked properly.

--- packages/distcc/distcc.zsh
+++ packages/distcc/distcc.zsh
@@ -1,8 +1,1 @@
-#!/bin/zsh
-# Forward script to Bash-based distcc.sh
-
-# Prepares running the build remotely. This is the entry point of the script.
-distcc_build() {
-    bash -c "source ~/.bash.d/distcc.sh; distcc_build $*"
-}
-
+source ~/.local/lib/distcc-driver/distcc.zsh
